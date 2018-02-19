b97881cf0099aa7736c150d54f0062d2c1438833
Joseph Orlando <josephorlando11@gmail.com>
2018-02-19 01:45:22 -0500

    Added a login page

--- webapp/public/index.php
+++ webapp/public/index.php
@@ -9,11 +9,11 @@
     </head>
     <body>
         <div class="login">
-            <h1>Login</h1>
+            <h1>Collange</h1>
             <form method="post">
-                <input type="text" name="u" placeholder="Username" required="required" />
+                <input type="text" name="u" placeholder="E-Mail Address" required="required" />
                 <input type="password" name="p" placeholder="Password" required="required" />
-                <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+                <button type="submit" class="btn btn-primary btn-block btn-large">Sign In</button>
             </form>
         </div>
         <script  src="js/login.js"></script>
