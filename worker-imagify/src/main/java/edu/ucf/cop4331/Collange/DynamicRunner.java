3692a8bc60c1bcfd8686b9cc234ba8fc73903386
Joseph Orlando <josephorlando11@gmail.com>
2018-03-21 21:19:07 -0400

    Merge branch 'master' of https://github.com/joeyx22lm/Collange
    # Conflicts:
    #	.gradle/4.0/fileContent/fileContent.lock
    #	.gradle/4.0/fileHashes/fileHashes.bin
    #	.gradle/4.0/fileHashes/fileHashes.lock
    #	.gradle/4.0/taskHistory/fileSnapshots.bin
    #	.gradle/4.0/taskHistory/taskHistory.bin
    #	.gradle/4.0/taskHistory/taskHistory.lock
    #	worker-imagify/build/libs/worker-imagify-1.0.0.jar
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.MainActivityTest.html
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.imagify.GrayScaleTransitionTest.html
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.imagify.PackagedImageExamples.html
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.imagify.PixelTest.html
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.imagify.RGBTransitionsTest.html
    #	worker-imagify/build/reports/tests/test/classes/edu.ucf.cop4331.Collange.imagify.RGBUtilityTest.html
    #	worker-imagify/build/reports/tests/test/index.html
    #	worker-imagify/build/reports/tests/test/packages/edu.ucf.cop4331.Collange.html
    #	worker-imagify/build/reports/tests/test/packages/edu.ucf.cop4331.Collange.imagify.html
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.MainActivityTest.xml
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.imagify.GrayScaleTransitionTest.xml
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.imagify.PackagedImageExamples.xml
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.imagify.PixelTest.xml
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.imagify.RGBTransitionsTest.xml
    #	worker-imagify/build/test-results/test/TEST-edu.ucf.cop4331.Collange.imagify.RGBUtilityTest.xml
    #	worker-imagify/build/test-results/test/binary/output.bin
    #	worker-imagify/build/test-results/test/binary/output.bin.idx
    #	worker-imagify/build/test-results/test/binary/results.bin

--- worker-imagify/src/main/java/edu/ucf/cop4331/Collange/DynamicRunner.java
+++ worker-imagify/src/main/java/edu/ucf/cop4331/Collange/DynamicRunner.java
@@ -5,21 +5,19 @@ import java.lang.reflect.InvocationTargetException;
 public class DynamicRunner {
     public static void main(String[] args, boolean exit){
         // Verify that some valid resource was given.
-        if(args != null && args.length > 0 && args[0] == null) {
-            if (args[0] != null) {
-                try {
-                    Class<?> type = Class.forName(args[0]);
-                    java.lang.reflect.Method mainMethod = type.getDeclaredMethod("main", String[].class);
-                    mainMethod.invoke(tail(args));
-                    if(exit) System.exit(0);
-                } catch (java.lang.ClassNotFoundException e) {
-                    System.out.println("Error: " + args[0] + " is not a valid resource: " + e.getMessage());
-                } catch (java.lang.NoSuchMethodException e) {
-                    System.out.println("Error: " + args[0] + " is not a runnable resource: " + e.getMessage());
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    System.out.println("An unexpected error occurred while running " + args[0] + ".main(String[]): " + e.getMessage());
-                    e.printStackTrace();
-                }
+        if(args != null && args.length > 0 && args[0] != null) {
+            try {
+                Class<?> type = Class.forName(args[0]);
+                java.lang.reflect.Method mainMethod = type.getDeclaredMethod("main", String[].class);
+                mainMethod.invoke(tail(args));
+                if(exit) System.exit(0);
+            } catch (java.lang.ClassNotFoundException e) {
+                System.out.println("Error: " + args[0] + " is not a valid resource: " + e.getMessage());
+            } catch (java.lang.NoSuchMethodException e) {
+                System.out.println("Error: " + args[0] + " is not a runnable resource: " + e.getMessage());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                System.out.println("An unexpected error occurred while running " + args[0] + ".main(String[]): " + e.getMessage());
+                e.printStackTrace();
             }
         }
 
