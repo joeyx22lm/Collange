7c1a831844aa6dc96624030e0023bbb86dc852d5
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 19:18:49 -0400

    changes

--- webapp/public/transform.php
+++ webapp/public/transform.php
@@ -68,8 +68,8 @@
                             <!--/.col-->
                         </div>
                         <div class="row">
-                            <div class="col-lg-12 img-responsive" id="canvas" style="margin: 0 auto;">
-                                <img src="https://placehold.it/2300x1268&text=No+Image" class="img" style="width:100%;"/>
+                            <div class="col-lg-12 img-responsive" id="canvas">
+                                <img src="https://placehold.it/2300x1268&text=No+Image" class="img" style="margin: 0 auto;width:100%;"/>
                             </div>
                         </div>
                     </div>
