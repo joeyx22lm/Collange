618eb1c9944582afc0dad2dafa7f5834062c66c0
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 19:39:55 -0400

    changes

--- webapp/public/transform.php
+++ webapp/public/transform.php
@@ -70,10 +70,10 @@ if(empty($TransformSession)){
             <div class="animated fadeIn">
                 <div class="row">
                     <div class="col-lg-12 img-responsive" id="canvas">
-                        <img src="https://placehold.it/2300x1268&text=<?php echo urlencode(<$TransformSession['imageName']);?>" class="img" style="margin: 0 auto;width:100%;padding:15px;"/>
+                        <img src="https://placehold.it/2300x1268&text=<?php echo urlencode($TransformSession['imageName']);?>" class="img" style="margin: 0 auto;width:100%;padding:15px;"/>
                     </div>
                 </div>
-                <div class="card" style="padding:15px;">
+                <div class="card" style="margin:15px;">
                     <div class="card-footer">
                         <ul>
                             <li>
