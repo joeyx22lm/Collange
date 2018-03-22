6a92ce34eb1021922c8675958fa013e83c388e7d
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 19:14:24 -0400

    changes

--- webapp/public/transform.php
+++ webapp/public/transform.php
@@ -76,38 +76,24 @@
                     <div class="card-footer">
                         <ul>
                             <li>
-                                <div class="text-muted">Visits</div>
-                                <strong>29.703 Users (40%)</strong>
+                                <div class="text-muted">Opacity</div>
+                                <strong>40%</strong>
                                 <div class="progress progress-xs mt-2">
                                     <div class="progress-bar bg-success" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </li>
                             <li class="d-none d-md-table-cell">
-                                <div class="text-muted">Unique</div>
-                                <strong>24.093 Users (20%)</strong>
+                                <div class="text-muted">Brightness</div>
+                                <strong>20%</strong>
                                 <div class="progress progress-xs mt-2">
                                     <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </li>
                             <li>
-                                <div class="text-muted">Pageviews</div>
-                                <strong>78.706 Views (60%)</strong>
+                                <div class="text-muted">Warmth</div>
+                                <strong>70%</strong>
                                 <div class="progress progress-xs mt-2">
-                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </li>
-                            <li class="d-none d-md-table-cell">
-                                <div class="text-muted">New Users</div>
-                                <strong>22.123 Users (80%)</strong>
-                                <div class="progress progress-xs mt-2">
-                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </li>
-                            <li class="d-none d-md-table-cell">
-                                <div class="text-muted">Bounce Rate</div>
-                                <strong>40.15%</strong>
-                                <div class="progress progress-xs mt-2">
-                                    <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </li>
                         </ul>
