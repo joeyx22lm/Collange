26a8bb95d45d0375294f9baadc8295202d035b25
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 18:55:25 -0400

    changes

--- webapp/lib/ui/component/navbar.php
+++ webapp/lib/ui/component/navbar.php
@@ -2,7 +2,7 @@
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <h2 style="padding-left:5px;">Collange</h2>
+    <image src="https://placehold.it/123x38&text=Collange" alt="Collange"/>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
         <span class="navbar-toggler-icon"></span>
     </button>
