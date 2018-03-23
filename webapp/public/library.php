820773cc143ba3738930b31557c749c23fe0fa6a
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 20:49:52 -0400

    changes

--- webapp/public/library.php
+++ webapp/public/library.php
@@ -380,21 +380,17 @@
     </aside>
 </div>
 
-<div class="modal" tabindex="-1" role="dialog">
+<div class="modal" id="processingModal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Uploading Image</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Save changes</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <p>Please wait while we upload your image</p>
             </div>
         </div>
     </div>
@@ -416,6 +412,9 @@
         });
         uploader.on('dragleave', function(e){
             libraryView.css('opacity', '1');
+        });
+        uploader.on('drop', function(e){
+            $('#processingModal').modal('show');
         });
     });
 </script>
