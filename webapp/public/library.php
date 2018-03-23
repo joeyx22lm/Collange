a1b7c8925115217562e38a160f3a4c5f651d2ce9
Joseph Orlando <josephorlando11@gmail.com>
2018-03-22 20:47:54 -0400

    changes

--- webapp/public/library.php
+++ webapp/public/library.php
@@ -379,13 +379,45 @@
         </div>
     </aside>
 </div>
+
+<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php App::buildPageFooter();?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.js"></script>
 <script>
     /**
      * Start Dropzone.JS
      */
-    var uploader = new Dropzone("div#library-view", { url: "/file/post"});
+    $(document).ready(function(){
+        var libraryView = $('#library-view');
+        var uploader = new Dropzone("div#library-view", { url: "/file/post"});
+
+        uploader.on('dragover', function(e){
+            libraryView.css('opacity', '0.25');
+        });
+        uploader.on('dragleave', function(e){
+            libraryView.css('opacity', '1');
+        });
+    });
 </script>
 </body>
 </html>
