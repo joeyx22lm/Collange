17053f8fa100cc04a84266609857960c6293939d
Joseph Orlando <josephorlando11@gmail.com>
2018-04-10 18:54:09 -0400

    made responsive

--- webapp/public/home.php
+++ webapp/public/home.php
@@ -57,16 +57,16 @@
             <div class="animated fadeIn">
 
                 <div class="row">
-                    <div class="col-lg-3 img-responsive">
+                    <div class="col-lg-3 col-md-6 col-xs-12 img-responsive">
                         <img src="https://placehold.it/300x300"/>
                     </div>
-                    <div class="col-lg-3 img-responsive">
+                    <div class="col-lg-3 col-md-6 col-xs-12 img-responsive">
                         <img src="https://placehold.it/300x300"/>
                     </div>
-                    <div class="col-lg-3 img-responsive">
+                    <div class="col-lg-3 col-md-6 col-xs-12 img-responsive">
                         <img src="https://placehold.it/300x300"/>
                     </div>
-                    <div class="col-lg-3 img-responsive">
+                    <div class="col-lg-3 col-md-6 col-xs-12 img-responsive">
                         <img src="https://placehold.it/300x300"/>
                     </div>
                 </div>
