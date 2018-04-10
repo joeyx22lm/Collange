8f0fabfb216eda693d5b5756e989cf2248ccbeab
Chadwick Maycumber <ChadwickMaycumber@Chads-Mac.local>
2018-04-10 18:52:52 -0400

    Removed unclickable icons on the nav menu

--- webapp/lib/ui/component/navbar.php
+++ webapp/lib/ui/component/navbar.php
@@ -19,15 +19,6 @@
         </li>
     </ul>-->
     <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-list"></i></a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
-        </li>
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 <img src="https://placehold.it/35x35" class="img-avatar" alt="<?php echo AuthSession::getUser()->getEmail();?>">
