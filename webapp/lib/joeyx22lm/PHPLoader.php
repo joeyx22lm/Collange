f02356ae284b221f92d78a0ce7067c8a62b4400a
Joseph Orlando <josephorlando11@gmail.com>
2018-04-14 15:42:57 -0400

    Debug statements.

--- webapp/lib/joeyx22lm/PHPLoader.php
+++ webapp/lib/joeyx22lm/PHPLoader.php
@@ -3,12 +3,15 @@
  * Class PHPLoader
  */
 class PHPLoader {
-    public static $modules = array();
+    public static $modules;
     public static function initModule($name, $resource=null){
         // Check if overriding all modules.
         if(is_array($name) && $resource == null){
             self::$modules = $name;
         }else{
+            if(self::$modules == null){
+                self::$modules = array();
+            }
             self::$modules[$name] = $resource;
         }
     }
@@ -22,6 +25,14 @@ class PHPLoader {
         // load a single resource.
         else if(isset(self::$modules[$name])){
             require_once(self::$modules[$name]);
+        }
+    }
+    public static function printModules($die=true){
+        if(self::$modules != null) {
+            foreach(self::$modules as $name=>$resource){
+                echo($name . ':\t\t' . $resource);
+            }
+            if($die) die();
         }
     }
 }
