3db0338c3410f31c93c84fd6397404e8bfd8c9e5
Joseph Orlando <josephorlando11@gmail.com>
2018-04-14 14:57:53 -0400

    Build UUID functionality and more S3 stuff.

--- webapp/public/api.php
+++ webapp/public/api.php
@@ -1,9 +1,11 @@
 <?php require_once('../Application.php');?>
 <?php AuthSession::protect();
-
-
-
 if(isset($_GET['signedKey'])){
-    die(S3Handler::createSignedPOSTUrl("testdocument.jpg", $_GET['mime']));
+    if(empty($_GET['mime']) || empty($_GET['ext'])){
+        // Incorrect input.
+        http_response_code(400);
+        die();
+    }
+    die(S3Handler::createSignedPOSTUrl(UUID::randomUUID() . " $_GET[ext]", $_GET['mime']));
 }
 ?>
--- webapp/public/js/views/main.js
+++ webapp/public/js/views/main.js
@@ -7,7 +7,6 @@ $(function(){
     var r = parseInt(hex.substring(0,2), 16);
     var g = parseInt(hex.substring(2,4), 16);
     var b = parseInt(hex.substring(4,6), 16);
-
     var result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
     return result;
   }
