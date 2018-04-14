62afbc4676ae69d1e72bfed6a5cb9090cf9fd9c8
Joseph Orlando <josephorlando11@gmail.com>
2018-04-14 16:36:20 -0400

    upload service

--- webapp/public/api.php
+++ webapp/public/api.php
@@ -17,4 +17,29 @@ if(isset($_GET['signedKey'])){
     PHPLoader::loadModule('Collange:S3Handler');
     die(S3Handler::createSignedPOSTUrl(UUID::randomUUID() . " $_GET[ext]", $_GET['mime']));
 }
+
+
+
+/**
+ * Handle User File Uploads
+ */
+if(isset($_GET['upload'])){
+    var_dump($_FILES);
+    die();
+    //die(json_encode($_FILES));
+   /* $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if(isset($_POST["submit"])) {
+       // $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }*/
+}
 ?>
