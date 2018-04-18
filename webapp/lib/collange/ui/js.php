4db32ea5d1f150d0130cad3f791bf44d58bb2523
Joseph Orlando <josephorlando11@gmail.com>
2018-04-18 17:41:51 -0400

    Updated bootstrap JS to v4.1

--- webapp/lib/collange/ui/js.php
+++ webapp/lib/collange/ui/js.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <script src="/js/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
 <script src="js/app.js"></script>
