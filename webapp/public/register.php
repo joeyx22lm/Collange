27a3c1a18d01bd534531a648171e55e31f9c1b9d
Chadwick Maycumber <ChadwickMaycumber@Chads-Mac.local>
2018-04-18 01:16:11 -0400

    User registration

--- webapp/public/register.php
+++ webapp/public/register.php
@@ -31,6 +31,7 @@ if (isset($_POST['register'])) {
 
               $sql = "INSERT INTO user (firstName, lastName, password, email) VALUES ('$firstname', '$lastname', '$password', '$email')";
 
+              print DBSession::getSession();
 
               if (DBSession::getSession()->query($sql)) {
                 print "This works";
