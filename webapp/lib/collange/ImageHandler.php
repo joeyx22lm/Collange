6dd1e316498d8e4e9d107091d792ece11c02e075
Joseph Orlando <josephorlando11@gmail.com>
2018-04-18 20:01:24 -0400

    composer.json

--- webapp/lib/collange/ImageHandler.php
+++ webapp/lib/collange/ImageHandler.php
@@ -18,18 +18,40 @@ class ImageHandler {
         unlink($src);
         return $output;
     }
+
+    /**
+     * @author xtempore at stackoverflow
+     * @param $src
+     */
     public static function stripEXIFFromJPEG($src){
-        try {
-            $img = new Imagick($src);
-            $img->stripImage();
-            $img->writeImage($src);
-            $img->clear();
-            $img->destroy();
-            return $src;
-        } catch(Exception $e) {
-            Log::error('stripEXIFFromJPEG('.$src.'): ' . $e->getMessage());
+        $new = '/tmp/'.UUID::randomUUID() . '.jpg';
+        $f1 = fopen($src, 'rb');
+        $f2 = fopen($new, 'wb');
+
+        // Find EXIF marker
+        while (($s = fread($f1, 2))) {
+            $word = unpack('ni', $s)['i'];
+            if ($word == 0xFFE1) {
+                // Read length (includes the word used for the length)
+                $s = fread($f1, 2);
+                $len = unpack('ni', $s)['i'];
+                // Skip the EXIF info
+                fread($f1, $len - 2);
+                break;
+            } else {
+                fwrite($f2, $s, 2);
+            }
         }
-        return null;
+
+        // Write the rest of the file
+        while (($s = fread($f1, 4096))) {
+            fwrite($f2, $s, strlen($s));
+        }
+
+        fclose($f1);
+        fclose($f2);
+        unlink($src);
+        return $new;
     }
 }
 ?>
