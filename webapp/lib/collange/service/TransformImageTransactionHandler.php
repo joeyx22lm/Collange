d1ff296503e01786f52859c69ec37ec6579687f9
Joseph Orlando <josephorlando11@gmail.com>
2018-04-19 00:41:42 -0400

    fix async message

--- webapp/lib/collange/service/TransformImageTransactionHandler.php
+++ webapp/lib/collange/service/TransformImageTransactionHandler.php
@@ -2,13 +2,13 @@
 class TransformImageRequestHandler extends RedisHandler {
     public static function enqueue($imagekey, $txId, $revisionId, $filter){
         $EventUUID = UUID::randomUUID();
-        if(self::getSession()->rpush(StaticResource::get('TransformWaitingQueue'), array(
+        if(self::getSession()->rpush(StaticResource::get('TransformWaitingQueue'), json_encode(array(
             'key'=>$imagekey,
             'transactionId'=>$txId,
             'revisionId'=>$revisionId,
             'filter'=>$filter,
             'eventId'=>$EventUUID
-        ))){
+        )))){
             return $EventUUID;
         }
         return null;
