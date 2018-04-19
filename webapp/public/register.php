1b959ef01869f04b2935c954a5d17aee96c371db
Joseph Orlando <josephorlando11@gmail.com>
2018-04-19 13:27:27 -0400

    fix user registration CSS/JS imports

--- webapp/public/register.php
+++ webapp/public/register.php
@@ -114,10 +114,7 @@ if (isset($_POST['register'])) {
     </div>
 </div>
 
-<!-- Bootstrap and necessary plugins -->
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
-<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<?php App::buildHtmlJS();?>
 
 </body>
 </html>
