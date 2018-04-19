f07c3db47c3ada205805d718d79b81efcd1a0ba0
Joseph Orlando <josephorlando11@gmail.com>
2018-04-19 13:26:46 -0400

    fix user registration

--- webapp/public/register.php
+++ webapp/public/register.php
@@ -34,28 +34,8 @@ if (isset($_POST['register'])) {
 }?><!DOCTYPE html>
 <html lang="en">
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="CoreUI Bootstrap 4 Admin Template">
-    <meta name="author" content="Lukasz Holeczek">
-    <meta name="keyword" content="CoreUI Bootstrap 4 Admin Template">
-    <!-- <link rel="shortcut icon" href="assets/ico/favicon.png"> -->
-
-    <title>CoreUI Bootstrap 4 Admin Template</title>
-
-    <!-- Icons -->
-    <link href="node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="node_modules/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
-
-    <!-- Main styles for this application -->
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- Styles required by this views -->
-
+    <?php App::buildHtmlHead('Register');?>
 </head>
-
 <body class="app flex-row align-items-center">
 <div class="container">
     <?php
