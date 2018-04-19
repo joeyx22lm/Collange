bfcd5c670fc349d9c9b2d5a16dfc899308c41f49
Joseph Orlando <josephorlando11@gmail.com>
2018-04-18 22:14:18 -0400

    fix popper.js for bootstrap

--- webapp/lib/collange/ui/js.php
+++ webapp/lib/collange/ui/js.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-<script src="/js/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
 <script src="js/app.js"></script>
